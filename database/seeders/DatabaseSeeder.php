b5db54f7bab807d0a04f5a0d483bc290c2b51985
AB SIDDIQUE <31104573+SiddiqueAb@users.noreply.github.com>
2026-05-06 21:36:52 +0600

    Task completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,35 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Product::updateOrCreate([
+            'title' => 'Adidas Running Shoes',
+        ],
+        [
+            'img' => 'https://m.media-amazon.com/images/I/71wqj+v9sUL._AC_UL320_.jpg',
+            'brand' => 'Adidas',
+            'rating' => 4.5,
+            'reviews' => 1200,
+            'sellPrice' => 79.99,
+            'orders' => '5000+',
+            'mrp' => '$99.99',
+            'discount' => 20,
+            'category' => 'men'
+        ]);
+
+        Product::updateOrCreate([
+            'title' => 'Puma Running Shoes',
+        ],
+        [
+            'img' => 'https://m.media-amazon.com/images/I/71wqj+v9sUL._AC_UL320_.jpg',
+            'brand' => 'Puma',
+            'rating' => 4.5,
+            'reviews' => 1200,
+            'sellPrice' => 79.99,
+            'orders' => '5000+',
+            'mrp' => '$99.99',
+            'discount' => 20,
+            'category' => 'women'
+        ]);
     }
 }
